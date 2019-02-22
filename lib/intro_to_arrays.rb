8a03b6df805a3f5ad9980d5242622a5e67bf37e9
stupendous-trainer-5541 <peter.c.viss@biola.edu>
2019-02-22 04:47:13 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -9,6 +9,7 @@ end
 taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of the Woods"]
 
 def first_element(my_first_element)
+  taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of the Woods"]
 my_first_element = taylor_swift.first
 
 end
