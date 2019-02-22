c9334a79a528400068a870a3bf8f9c8a3f5e1158
stupendous-trainer-5541 <peter.c.viss@biola.edu>
2019-02-22 04:38:15 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -6,6 +6,10 @@ def array_with_two_elements
   my_two_array = [1,2]
 end
 
+taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of the Woods"]
+
 def my_first_element
 my_two_array.shift
 end
+
+
